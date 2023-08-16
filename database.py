e289d5e83126e2b2473a9c540769c1c001361a1c
utkarsh <sutkarsh6000@gmail.com>
2023-08-16 20:11:04 +0530

    added icon and some styling

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine,text,insert
+
 import os 
 
 db_connection_string= os.environ['DB_CONNECTION_STRING']
@@ -37,7 +38,7 @@ def add_application_to_db(job_id, data):
         query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
     
         conn.execute(query, 
-                     {"job_id": job_id, 
+                      {"job_id": job_id, 
                       "full_name": data['full_name'],
                       "email": data['email'],
                       "linkedin_url": data['linkedin_url'],
